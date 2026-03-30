0082602d62368d4d5d95e4efddbd056443ce32a0
liuc81361-creator <liuc81361@gmail.com>
2026-03-30 19:53:57 +0800

    Add files via upload

---
+++ 2.7练习/2.7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	/*cout << "myname\n"
+		<< "local" << endl;*/
+	cout << "Plese write a distance: ";
+	int distance;
+	cin >> distance;
+	int ma = distance * 220;
+	cout << "there have " << ma << " ma" << endl;
+}
